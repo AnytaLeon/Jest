60a88531a748f531bed7c3df4ef646fad2832ae3
Anna Petelina <petelina_an@mail.ru>
2025-01-02 17:31:26 +0300

    исправила ошибки eslint

--- src/calculate.js
+++ src/calculate.js
@@ -1,4 +1,4 @@
-export function calculateTotal(purchases, applyDiscount) {
+export default function calculateTotal(purchases, applyDiscount) {
   const sum = purchases.reduce((acc, item) => acc + (item.count * item.price), 0);
   if (applyDiscount) {
     return sum * 0.891;
